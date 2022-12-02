ddd02385da43091a0e10c1538f0405c7b23b6fbd
ThamerAyachi <thamerjak3@gmail.com>
2022-12-02 10:50:07 +0100

    add head file

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#endif
