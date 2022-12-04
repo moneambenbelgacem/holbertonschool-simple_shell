c785102c7b38819165956fe28d6c973a226ed19d
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-12-04 20:25:30 +0100

    change main to base

--- main.c
+++ main.c
@@ -1,12 +1,179 @@
-#include "shell.h"
-int main(int argc, char **argv)
+#include <stdio.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <wait.h>
+
+#define SENTENCE_LEN 511
+#define PATH_MAX 512
+
+void printPrompt();
+char *getUserName();
+int numOfWords(const char sentence[]);
+void parseString(char sentence[], char **parsedStr);
+void freeArr(char **parsedStr);
+void exeCommand(char **command);
+void donePrint(int numOfCommands, int lengthOfCommands);
+
+int main()
 {
-  // Load config files, if any.
 
-  // Run command loop.
-  lsh_loop();
+	char sentence[SENTENCE_LEN];
+	char **parsedStr;
+	int numOfCommands = 0, lengthOfCommands = 0, parsedStrLen;
+	pid_t id;
 
-  // Perform any shutdown/cleanup.
+	while (1)
+	{
+		printPrompt();
+		if (fgets(sentence, SENTENCE_LEN, stdin) != NULL)
+		{
+			parsedStrLen = numOfWords(sentence);
+			lengthOfCommands += (int)(strlen(sentence) - 1);
+			numOfCommands++;
+			if (parsedStrLen > 0)
+			{
+				parsedStr = (char **)malloc((parsedStrLen + 1) * sizeof(char *));
+				if (parsedStr == NULL)
+				{
+					fprintf(stderr, "malloc failed");
+					exit(1);
+				}
+				parsedStr[parsedStrLen] = NULL;
+				parseString(sentence, parsedStr);
+				if (strcmp(parsedStr[0], "done") == 0 && parsedStrLen == 1)
+				{
+					donePrint(numOfCommands, lengthOfCommands);
+					freeArr(parsedStr);
+					break;
+				}
+				id = fork();
+				if (id < 0)
+				{
+					perror("ERR");
+					freeArr(parsedStr);
+					exit(1);
+				}
+				else if (id == 0)
+				{
+					exeCommand(parsedStr);
+					freeArr(parsedStr);
+				}
+				else
+				{
+					wait(NULL);
+					freeArr(parsedStr);
+				}
+			}
+		}
+	}
+	return 0;
+}
 
-  return EXIT_SUCCESS;
+
+void printPrompt()
+{
+	char *userName = getUserName();
+	char currentDir[PATH_MAX];
+
+	getcwd(currentDir, sizeof(currentDir));
+	printf("%s@%s>", userName, currentDir);
+}
+
+char *getUserName()
+{
+	uid_t uid = geteuid();
+	struct passwd *pw = getpwuid(uid);
+	if (pw)
+		return pw->pw_name;
+	else
+	{
+		return NULL;
+	}
+}
+
+int numOfWords(const char sentence[])
+{
+	int i = 0, wordCounter = 0;
+	while (sentence[i] != '\n')
+	{
+		if (sentence[i] != ' ' && (sentence[i + 1] == ' ' || sentence[i + 1] == '\n'))
+			wordCounter++;
+		i++;
+	}
+	return wordCounter;
+}
+
+/*receives a sentence as a char[] and a char**
+ *assign dynamically the sentence words into the char**
+ */
+void parseString(char sentence[], char **parsedStr)
+{
+	char tmpWord[SENTENCE_LEN];
+	int tmpIndex = 0, parsedIndex = 0;
+	size_t i;
+	for ( i = 0; i < strlen(sentence); i++)
+	{
+		if (sentence[i] != ' ' && sentence[i] != '\n')
+		{
+			tmpWord[tmpIndex] = sentence[i];
+			tmpIndex++;
+		}
+		
+		else if ((sentence[i] == ' ' || sentence[i] == '\n') && tmpIndex > 0)
+		{
+			tmpWord[tmpIndex] = '\0';
+			parsedStr[parsedIndex] = (char *)malloc((strlen(tmpWord)) + 1);
+			if (parsedStr[parsedIndex] == NULL)
+			{
+				freeArr(parsedStr);
+				fprintf(stderr, "malloc failed");
+				exit(1);
+			}
+			strcpy(parsedStr[parsedIndex], tmpWord);
+			parsedIndex++;
+			tmpIndex = 0;
+		}
+	}
+}
+
+
+void freeArr(char **parsedStr) 
+{
+	int i = 0;
+	while (parsedStr[i])
+		free(parsedStr[i++]);
+	free(parsedStr);
+}
+
+
+void exeCommand(char **command)
+{
+	if (strcmp(command[0], "cd") == 0)
+	{
+		printf("command not supported (YET)\n");
+		freeArr(command);
+		exit(0);
+	}
+	else if (execvp(command[0], command) != -1)
+	{
+		freeArr(command);
+		exit(0);
+	}
+	else
+	{
+		perror("command not found");
+		freeArr(command);
+		exit(1);
+	}
+}
+
+
+void donePrint(int numOfCommands, int lengthOfCommands)
+{
+	printf("Num of commands: %d\n", numOfCommands);
+	printf("Total length of all commands: %d\n", lengthOfCommands);
+	printf("Average length of all commands: %f\n", (double)(lengthOfCommands) / numOfCommands);
+	printf("See you Next time !\n");
 }
