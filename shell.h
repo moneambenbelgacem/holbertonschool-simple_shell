575172dd1fe795cb9218bd039d1bcc3e5c807bc4
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-12-05 10:49:39 +0100

    modify

--- shell.h
+++ shell.h
@@ -1,35 +1,22 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-#define LSH_TOK_BUFSIZE 64
-#define LSH_TOK_DELIM " \t\r\n\a"
-#define _POSIX_SOURCE
-#include <stddef.h>
+#define SENTENCE_LEN 511
+#define PATH_MAX 512
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <pwd.h>
+#include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <time.h>
-
-int (*builtin_func[]) (char **) = {
-  &lsh_cd,
-  &lsh_help,
-  &lsh_exit
-};
-char *builtin_str[] = {
-  "cd",
-  "help",
-  "exit"
-};
-
-void lsh_loop(void);
-char *lsh_read_line(void);
-char **lsh_split_line(char *line);
-int lsh_launch(char **args);
-int lsh_cd(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **args);
-int lsh_num_builtins();
-int lsh_execute(char **args);
+#include <wait.h>
+void printPrompt();
+char *getUserName();
+int numOfWords(const char sentence[]);
+void parseString(char sentence[], char **parsedStr);
+void freeArr(char **parsedStr);
+void exeCommand(char **command);
+void donePrint(int numOfCommands, int lengthOfCommands);
 #endif
